2fe0e6d00ecb9221255433442947832f257de8a4
Ruslan Kadyrov <kadyrov96@mail.ru>
2018-11-05 14:27:43 +0300

    Start implementing main scheduler
    Start work on checking mail dirs for some new messages

---
+++ smtp_proj/client/include/scheduler.h
@@ -0,0 +1,14 @@
+#ifndef _SCHEDULER_H_
+#define _SCHEDULER_H_
+
+#include "server_types.h"
+
+#include <stdbool.h>
+#include <netdb.h>
+#include <sys/socket.h>
+
+
+int run_client();
+int check_output_mails();
+
+#endif // _SCHEDULER_H_
