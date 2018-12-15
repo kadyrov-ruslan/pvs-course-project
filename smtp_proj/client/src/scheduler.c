f9cd05510641d7c733381169fdc8cec8deb19602
dev <dev@debian>
2018-12-15 23:18:58 +0300

    Refactoring

--- smtp_proj/client/src/scheduler.c
+++ smtp_proj/client/src/scheduler.c
@@ -2,21 +2,16 @@
 #include <time.h>
 
 #define PROC_NUM 2
+#define MAX_MAIL_DOMAIN_NUM 50
 
-//в родительском процессе необходимо иметь дескрипторы дочерних процессов,
-//которые будут содержать число почтовых доменов и переданных писем в каждый дочерний процесс
-
-//todo реализовать функцию по отслеживанию, в какой процесс лучше отправить почтовый домен
 //autofsm для smtp протокола
 
-struct mail_domain_dscrptr mail_domains_dscrptrs[60];
-struct domain_mails domains_mails[60];
-int domains_count = 0;
-int ready_domains_count = 0;
-struct mail_process_dscrptr mail_procs[PROC_NUM];
+int ready_domains_count = 0; // юзается в чайлд процессах
 
 int run_client()
 {
+    // Дескрипторы дочерних процессов, содержат число почтовых доменов и переданных писем в каждый дочерний процесс
+    struct mail_process_dscrptr mail_procs[PROC_NUM];
     mail_procs[0].pid = fork();
     if (mail_procs[0].pid == 0)
         child_process_worker_start(6);
@@ -26,15 +21,18 @@ int run_client()
         if (mail_procs[1].pid == 0)
             child_process_worker_start(7);
         else
-            master_process_worker_start();
+            master_process_worker_start(mail_procs);
     }
 
     return 1;
 }
 
 // Содержит бизнес логику, обрабатываемую главным процессом
-int master_process_worker_start()
+int master_process_worker_start(struct mail_process_dscrptr *mail_procs)
 {
+    struct domain_mails domains_mails[MAX_MAIL_DOMAIN_NUM * 2];
+    int domains_count = 0;
+
     key_t key = ftok("/tmp", 6);
     mail_procs[0].msg_queue_id = msgget(key, 0666 | IPC_CREAT);
 
@@ -43,11 +41,15 @@ int master_process_worker_start()
 
     while (1)
     {
-        domains_count = get_domains_mails(domains_mails);
+        domains_count = get_domains_mails(domains_mails, domains_count);
         for (int i = 0; i < domains_count; i++)
         {
             for (int j = 0; j < domains_mails[i].mails_count; j++)
             {
+                //TODO реализовать функцию по отслеживанию, в какой процесс лучше отправить почтовый домен
+                //чекаем, в каком из процессов может находиться домен.
+                //если нашли, то скидываем в соответствующую очередь
+                //если нет, то скидываем туда, где меньше всего нагрузка
                 struct queue_msg new_msg;
                 new_msg.mtype = 1;
                 strcpy(new_msg.mtext, domains_mails[i].mails_paths[j]);
@@ -61,7 +63,7 @@ int master_process_worker_start()
             memset(&domains_mails[i].mails_paths[0], 0, sizeof(domains_mails[i].mails_paths));
         }
 
-        waitFor(10);
+        wait_for(10);
     }
     return 1;
 }
@@ -69,6 +71,8 @@ int master_process_worker_start()
 // Содержит бизнес логику, обрабатываемую отдельным процессом
 int child_process_worker_start(int proc_idx)
 {
+    struct mail_domain_dscrptr mail_domains_dscrptrs[MAX_MAIL_DOMAIN_NUM];
+
     fd_set read_fds;
     fd_set write_fds;
     fd_set except_fds;
@@ -86,7 +90,7 @@ int child_process_worker_start(int proc_idx)
         {
             if (strlen(cur_msg.mtext) != 0)
             {
-                process_email(cur_msg.mtext);
+                register_new_email(cur_msg.mtext, mail_domains_dscrptrs);
                 // int maxfd = 100;
                 // int activity = select(maxfd + 1, &read_fds, &write_fds, &except_fds, NULL);
                 // switch (activity)
@@ -140,7 +144,7 @@ int child_process_worker_start(int proc_idx)
 
 // Обновляет массив с описаниями зарегистрированных почтовых доменов
 // Каждый элемент содержит название домена, число новых писем для него и пути к письмам
-int get_domains_mails(struct domain_mails *domains_mails)
+int get_domains_mails(struct domain_mails *domains_mails, int domains_count)
 {
     struct dirent *user_dir;
     DIR *mail_dir = opendir(conf.mail_dir);
@@ -245,7 +249,8 @@ int get_domains_mails(struct domain_mails *domains_mails)
     return domains_count;
 }
 
-int process_email(char *email_path)
+// Регистрирует новое письмо в массиве дескрипторов mail_domains_dscrptrs для последующей обработки
+int register_new_email(char *email_path, struct mail_domain_dscrptr *mail_domains_dscrptrs)
 {
     printf("EMAIL %s\n", email_path);
     char *saved_email_path = malloc(strlen(email_path));
@@ -268,8 +273,6 @@ int process_email(char *email_path)
     free(tokens);
 
     //Проверяем, является ли домен письма новым
-    //Если да - создаем элемент массива дескриптора биндим сокет
-    //Если нет, считываем файл
     int found_domain_num = -1;
     for (int i = 0; i < ready_domains_count; i++)
     {
@@ -305,8 +308,9 @@ int process_email(char *email_path)
         else
         {
             printf("\n SUCCESS : Connected \n");
+
             //fflush(stdout);
-            mail_domains_dscrptrs[ready_domains_count].socket_fd = cur_domain_socket_fd;
+            //mail_domains_dscrptrs[ready_domains_count].socket_fd = cur_domain_socket_fd;
         }
 
         ready_domains_count++;
@@ -342,123 +346,10 @@ int process_email(char *email_path)
     return 1;
 }
 
-int process_output_mails()
+void wait_for(unsigned int secs)
 {
-    int domains_count = 0;
-
-    //run through users direrctories
-    struct dirent *user_dir;
-    DIR *mail_dir = opendir(conf.mail_dir);
-    if (mail_dir == NULL)
-        printf("Could not open MAIL directory");
-
-    while ((user_dir = readdir(mail_dir)) != NULL)
-    {
-        char *user_dir_name = user_dir->d_name;
-        if (strcmp(user_dir_name, ".") != 0 && strcmp(user_dir_name, "..") != 0)
-        {
-            char *user_dir_full_path = malloc(strlen(conf.mail_dir) + 2 + strlen(user_dir_name));
-            strcpy(user_dir_full_path, conf.mail_dir);
-            strcat(user_dir_full_path, user_dir_name);
-            strcat(user_dir_full_path, "/");
-
-            char *user_dir_new = malloc(strlen(user_dir_full_path) + 4);
-            strcpy(user_dir_new, user_dir_full_path);
-            strcat(user_dir_new, "new/");
-
-            DIR *new_dir = opendir(user_dir_new);
-            if (new_dir == NULL)
-                printf("Could not open NEW directory");
-
-            int mails_count = count_dir_entries(user_dir_new);
-            if (mails_count > 0)
-            {
-                char *email_full_names[mails_count];
-                char *email_short_names[mails_count];
-                int mail_names_count = 0;
-
-                printf("directory is NOT EMPTY\n");
-                struct dirent *new_entry;
-                while ((new_entry = readdir(new_dir)) != NULL)
-                {
-                    if (strcmp(new_entry->d_name, ".") != 0 && strcmp(new_entry->d_name, "..") != 0)
-                    {
-                        char *email_full_name = malloc(strlen(user_dir_new) + strlen(new_entry->d_name));
-                        strcpy(email_full_name, user_dir_new);
-                        strcat(email_full_name, new_entry->d_name);
-
-                        email_full_names[mail_names_count] = email_full_name;
-                        email_short_names[mail_names_count] = new_entry->d_name;
-                        mail_names_count++;
-                        //free(email_full_name);
-                    }
-                }
-
-                //Обрабатываем каждое письмо - чтение в память, отправка и rename()
-                for (int j = 0; j < mails_count; j++)
-                {
-                    //printf("EMAIL FULL NAME %s\n", email_full_names[j]);
-                    //printf("EMAIL SHORT NAME %s\n", email_short_names[j]);
-                    char *email_name = malloc(strlen(email_short_names[j]) + 1);
-                    strcpy(email_name, email_short_names[j]);
-
-                    char **tokens;
-                    tokens = str_split(email_name, '.');
-
-                    char *first_part = tokens[2];
-                    char *second_part = tokens[3];
-
-                    char *tmp_cur_mail_domain = malloc(strlen(first_part) + strlen(second_part) + 1);
-                    strcpy(tmp_cur_mail_domain, first_part);
-                    strcat(tmp_cur_mail_domain, ".");
-                    strcat(tmp_cur_mail_domain, second_part);
-                    free(tokens);
-
-                    tokens = str_split(tmp_cur_mail_domain, ',');
-                    char *cur_email_domain = tokens[0];
-                    free(tokens);
-
-                    for (int i = 0; i < ready_domains_count; i++)
-                    {
-                        if (strcmp(mail_domains_dscrptrs[i].domain, cur_email_domain) == 0)
-                        {
-                            printf("EMAIL DOMAIN %s\n", cur_email_domain);
-                            printf("EMAIL NAME %s\n", email_short_names[j]);
-                            char *email_msg = read_msg_file(email_full_names[j]);
-                            printf("EMAIL FILE MSG %s\n", email_msg);
-
-                            printf("SENDING DATA . . . \n");
-                            send_msg_to_server(mail_domains_dscrptrs[i].socket_fd, email_msg);
-
-                            //printf("NEW ENTRY %s\n", email_full_names[j]);
-                            char *email_new_name = str_replace(email_full_names[j], "new", "cur");
-                            //printf("ENTRY NEW NAME %s\n", email_new_name);
-
-                            int ret;
-                            ret = rename(email_full_names[j], email_new_name);
-                            if (ret == 0)
-                                printf("File renamed successfully\n");
-                            else
-                                printf("Error: unable to rename the file\n");
-
-                            break;
-                        }
-                    }
-                }
-            }
-            else
-                printf("directory is EMPTY\n");
-
-            closedir(new_dir);
-        }
-    }
-    closedir(mail_dir);
-    return domains_count;
-}
-
-void waitFor(unsigned int secs)
-{
-    unsigned int retTime = time(0) + secs; // Get finishing time.
+    // Get finishing time.
+    unsigned int retTime = time(0) + secs;
     while (time(0) < retTime)
         ; // Loop until it arrives.
 }
