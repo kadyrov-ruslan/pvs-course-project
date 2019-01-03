9339804cbffa7be69d1df674f1cf801473576fa5
dev <dev@debian>
2019-01-03 23:11:24 +0300

    Start each domain processing implementation

--- smtp_proj/client/include/scheduler.h
+++ smtp_proj/client/include/scheduler.h
@@ -1,19 +1,21 @@
 #ifndef _SCHEDULER_H_
 #define _SCHEDULER_H_
 
-#include "../include/map.h"
 #include "../../common/include/dir_utils.h"
 #include "../../common/include/string_utils.h"
 #include "../include/mx_utils.h"
 #include "../include/smtp_client.h"
 #include "../include/client_types.h"
 #include "../include/msg.h"
+#include "../include/map.h"
 #include <sys/select.h>
 #include <sys/wait.h>
 #include <sys/ipc.h> 
 #include <sys/msg.h> 
+#include <time.h>
 
-#define MAX_FD_CNT 1024
+#define PROC_NUM 2
+#define MAX_MAIL_DOMAIN_NUM 50
 
 int run_client();
 int master_process_worker_start(struct mail_process_dscrptr *mail_procs);
@@ -23,7 +25,10 @@ int get_domains_mails(struct domain_mails *domains_mails, int domains_count);
 int register_new_email(char *email_path, struct mail_domain_dscrptr *mail_domains_dscrptrs,
     fd_set *read_fds, fd_set *write_fds, fd_set *except_fds);
 
+void process_mail_domain(int maxfd, struct mail_domain_dscrptr cur_mail_domain,
+    fd_set *read_fds, fd_set *write_fds, fd_set *except_fds);    
+
 void wait_for (unsigned int secs);
 void shutdown_properly(int code);
 
-#endif // _SCHEDULER_H_
+#endif
