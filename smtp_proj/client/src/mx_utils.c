aa0f42b60c24125d9049fda87868df2d7718958a
dev <dev@debian>
2019-01-09 12:17:32 +0300

    Testing & fix solution

--- smtp_proj/client/src/mx_utils.c
+++ smtp_proj/client/src/mx_utils.c
@@ -21,6 +21,11 @@ struct sockaddr_in get_domain_server_info(char *domain_name)
     mail_server.sin_family = AF_INET; //AF_INIT means Internet doamin socket.
     mail_server.sin_port = htons(25); //port 25=SMTP.
     bcopy((char *)mail_info->h_addr_list[0], (char *)&mail_server.sin_addr.s_addr, mail_info->h_length);
+    char *IPbuffer = inet_ntoa(*((struct in_addr *)
+                                     mail_info->h_addr_list[0]));
+
+    printf("Host IP: %s", IPbuffer);
+
     return mail_server;
 }
 
@@ -31,6 +36,9 @@ char *get_domain_mx_server_name(char *domain_name)
     ns_msg msg;
     ns_rr rr;
     int r;
+
+    if (strcmp(domain_name, "localhost.com") == 0)
+        return "localhost";
 
     // MX RECORD
     r = res_query(domain_name, ns_c_any, ns_t_mx, nsbuf, sizeof(nsbuf));
@@ -52,6 +60,5 @@ char *get_domain_mx_server_name(char *domain_name)
             ns_name_ntop(name, mx_server, 4096);
         }
     }
-
     return mx_server;
 }
